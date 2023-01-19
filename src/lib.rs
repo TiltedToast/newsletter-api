bfcfb369ebcef9663b3c26c69b85c56945884e18
Tim <57221600+TiltedToast@users.noreply.github.com>
2023-01-19 23:49:48 +0100

    mock out endpoint for subscribing to the newsletter

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,33 @@
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{
+    web::{get, post, Form},
+    App, HttpResponse, HttpServer,
+};
+use serde::Deserialize;
 use std::net::TcpListener;
+
+#[derive(Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
+async fn subscribe(form: Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", get().to(health_check))
+            .route("/subscriptions", post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
