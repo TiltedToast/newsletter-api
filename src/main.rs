fa6d50896daa3382c1ba2a9b14e2762a366b3b0c
Tim <57221600+TiltedToast@users.noreply.github.com>
2023-01-19 23:04:02 +0100

    run test on random available port

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use newsletter_api::run;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run()?.await
+    run(TcpListener::bind("127.0.0.1:8000").expect("Failed to bind port 8000"))?.await
 }
